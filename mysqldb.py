dd29563199116f3c5015ba35d3a3b1953c54ba2b
Hyphersism <nguyenphat0804@gmail.com>
2021-07-05 16:32:54 +0700

    MySQL Database

--- mysqldb.py
+++ mysqldb.py
@@ -1,19 +1,80 @@
 import mysql.connector
 
-try:
-  mydb = mysql.connector.connect(
-    host="localhost",
-    database="mysqldb",
-    user="root",
-    password="ba4569852",
-    port=3308
-  )
+class MysqlDB:
+  def __init__(self, coin_name) -> None:
+      self.cursor = None
+      self.coin_name = coin_name
 
-  mycursor = mydb.cursor()
+  def connect_db(self) -> None:
+    '''
+    Connect to database MySQL.
+    '''
 
-  mycursor.execute("SHOW DATABASES")
+    try:
+      self.mydb = mysql.connector.connect(
+            host="localhost",
+            database="mysqldb",
+            user="root",
+            password="ba4569852",
+            port=3308
+      )
 
-  for x in mycursor:
-    print(x)
-except:
-  print("Cannot connect to MySQL")
+      self.cursor = self.mydb.cursor()
+
+    except Exception as e:
+      print(e)
+
+  def close_db(self) -> None:
+    '''
+    Close database.
+    '''
+
+    self.mydb.close()
+
+  def create_table(self) -> None:
+    '''
+    Create new table by coin's name.
+    Delete table if it already exists.
+    '''
+
+    try:
+      self.cursor.executemany(f"DROP TABLE IF EXISTS {self.coin_name}")
+
+      self.cursor.execute(f'''CREATE TABLE {self.coin_name} (
+            name VARCHAR(255),
+            symbol VARCHAR(255),
+            date DATE,
+            open FLOAT,
+            high FLOAT,
+            low FLOAT,
+            close FLOAT,
+            volume FLOAT)'''
+      )
+
+      print(f'Create Table {self.coin_name} Successfully')
+
+    except Exception as e:
+      print(e)
+
+  def insert(self, data: list) -> None:
+    '''
+    Insert data to table.
+    '''
+
+    try:
+      self.cursor.execute(f'''INSERT INTO {self.coin_name} (name, symbol, date, open, high, low, close, volume)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'''
+      )
+
+      self.mydb.commit()
+
+      print(self.cursor.rowcount, "record inserted.")
+
+    except Exception as e:
+      print(e)
+
+  def update(self) -> None:
+    '''
+    Update new data to table.
+    '''
+    pass
