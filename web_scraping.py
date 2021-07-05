8f87a29f0caa16c65ae7ca92f25593e33419c660
Hyphersism <nguyenphat0804@gmail.com>
2021-07-05 19:29:20 +0700

    Navigate Website

--- web_scraping.py
+++ web_scraping.py
@@ -1,67 +1,49 @@
-import urllib
-import urllib.request
-import re
-
-from datetime import datetime, timedelta
+from datetime import datetime
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from navigate_website import NavigateWebsite
+from mysqldb import MysqlDB
 
-class NavigateWebsite:
-  def __init__(self, url: str, coin_list: list) -> None:
-    self.url = url
-    self.coin_list = coin_list
-    self.chrome_options = Options()
+class WebScraping:
+  def __init__(self, coin_list: list) -> None:
+      self.coin_list = coin_list
+      self.page_sources = NavigateWebsite(coin_list)
 
-  def get_input_date(self) -> None:
-    self.end = datetime.today().date() - timedelta(days=1)
-    self.start = (self.end - timedelta(days=365)).strftime(r'%d-%m-%Y')
-    self.end = self.end.strftime(r'%d-%m-%Y')
+  def __float__(self, number: str) -> float:
+    try:
+      return float(''.join(number.split(',')))
+    except:
+      return number
 
-  def get_options(self) -> None:
-    self.chrome_options.add_argument('--ignore-certificate-errors')
-    self.chrome_options.add_argument('--incognito')
-    self.chrome_options.add_argument('--headless')
+  def format_date(self, date: str) -> str:
+    '''
+    Format date 
+    '''
 
-  def get_driver(self) -> None:
-    self.driver = webdriver.Chrome(options=self.chrome_options,
-                                   executable_path=r'..\\Project\\WebDriver\\chromedriver.exe')
-    self.driver.get(self.url)
+    date = datetime.strptime(date, r'%b %d, %Y')
+    date = datetime.strftime(r'%d-%m-%Y')
+    return date
 
-  def get_page_source(self) -> list:
-    page_sources = {}
-
-    self.get_input_date()
-    self.get_options()
-    self.get_driver()
+  def scarping(self):
+    '''
+    Getting and storing data of cryptocurrencies from website
+    '''
 
     for coin in self.coin_list:
-      self.driver.find_element_by_xpath(f'//*[@title="{coin}"]').click()
-      self.driver.implicitly_wait(1)
+      soup = BeautifulSoup(self.page_sources[coin], 'lxml')
 
-      historial_data_coin = self.driver.find_element_by_link_text('Historical Data').click()
-
-      self.driver.implicitly_wait(1)
-      input_date = self.driver.find_element_by_xpath('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/div[1]/div/div/div/span').click()
-
-      start_date = self.driver.find_element_by_xpath('//*[@name="startDate"]')
-      start_date.send_keys(self.start)
-      start_date.send_keys(Keys.RETURN)
-
-      end_date = self.driver.find_element_by_xpath('//*[@name="endDate"]')
-      end_date.send_keys(self.end)
-      end_date.send_keys(Keys.RETURN)
-
-      apply_button = self.driver.find_element_by_xpath('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/button').click()
-
-      # Scroll to the end of page
-      for _ in range(50):
-        html = self.driver.find_element_by_tag_name('html')
-        html.send_keys(Keys.PAGE_DOWN)
-
-      page_sources[coin] = self.driver.page_source
-
-      self.driver.find_element_by_xpath('//*[@title="Cryptocurrencies"]').click()
-
+      symbol = soup.find('h1').text.split()[2][1:-1]
+      tbody = soup.find('tbody').find_all('tr')
       
+      data_list = []
+      for data in tbody:
+        row = [coin, symbol] + [coin_data.text for idx, coin_data in enumerate(data) if idx != 5]
+        row[2] = self.format_date(row[2])
+        row[3:] = map(float, row[3:])
+        data_list.append(tuple(row))
+
+      # Insert data to table
+      table = MysqlDB(coin)
+      table.connect_db()
+      table.create_table()
+      table.insert(data_list)
+      table.close_db()
