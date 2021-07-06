6e0b21743854067e5ecc520e93215c1198452edf
Hyphersism <nguyenphat0804@gmail.com>
2021-07-06 14:33:04 +0700

    Add coin Litecoin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from web_scraping import WebScraping
 
-coin_list = ['Bitcoin', 'Ethereum', 'Tether']
-
 if __name__ == "__main__":
+  coin_list = ['Bitcoin', 'Ethereum', 'Tether', 'Litecoin']
   WebScraping(coin_list).scarping()
